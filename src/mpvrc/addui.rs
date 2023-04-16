ec65925793ccae414a3776b83da849a15df63ef3
c000 <c000@users.noreply.github.com>
2023-04-16 16:48:28 +0900

    Enter to add

--- src/mpvrc/addui.rs
+++ src/mpvrc/addui.rs
@@ -21,19 +21,27 @@ impl AddUi {
     pub fn ui(&mut self, ui: &mut egui::Ui) -> AddUiResult {
         let mut result = AddUiResult::Nop;
 
+        let mut enter = false;
+
         ui.horizontal(|ui| {
             ui.label("Named pipe path:");
         });
         ui.vertical_centered_justified(|ui| {
-            egui::TextEdit::singleline(&mut self.named_pipe_path)
+            let input = egui::TextEdit::singleline(&mut self.named_pipe_path)
                 .hint_text(DEFAULT_PATH)
                 .show(ui);
+
+            enter |= input.response.lost_focus() && ui.input(|i| i.key_down(egui::Key::Enter));
         });
         ui.with_layout(egui::Layout::right_to_left(egui::Align::TOP), |ui| {
             if ui.button("add").clicked() {
-                result = AddUiResult::Add(self.named_pipe_path.clone());
+                enter |= true;
             }
         });
+
+        if enter {
+            result = AddUiResult::Add(self.named_pipe_path.clone());
+        }
 
         result
     }
