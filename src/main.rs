6c303862990a5a6c74936c0487f8b6a04166cdbf
c000 <c000@users.noreply.github.com>
2024-09-21 02:02:07 +0900

    Fixed for updates

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ mod mpvrc;
 
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(640.0, 480.0)),
+        viewport: egui::ViewportBuilder::default().with_inner_size(egui::vec2(640.0, 480.0)),
         ..Default::default()
     };
     eframe::run_native(
         "mpv Remote Controller",
         options,
-        Box::new(|_cc| Box::new(mpvrc::App::default())),
+        Box::new(|_cc| Ok(Box::new(mpvrc::App::default()))),
     )
 }
