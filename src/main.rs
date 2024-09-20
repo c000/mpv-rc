bd9ebfb239dda480dbe76551b66cb0d09ea13238
c000 <c000@users.noreply.github.com>
2024-09-21 02:48:04 +0900

    Show version on title

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #![windows_subsystem = "windows"]
 mod mpvrc;
 
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size(egui::vec2(640.0, 480.0)),
         ..Default::default()
     };
     eframe::run_native(
-        "mpv Remote Controller",
+        &format!("mpv Remote Controller v{}", VERSION),
         options,
         Box::new(|_cc| Ok(Box::new(mpvrc::App::default()))),
     )
